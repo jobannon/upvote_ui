42ed2f8ffa20fe23a0b003c595625b6add45c4a8
Scott Regenthal <scottregenthal@gmail.com>
2020-02-24 10:22:31 -0700

    Remove route

--- upvote.rb
+++ upvote.rb
@@ -15,17 +15,17 @@ get '/ideas/new' do
   erb :new
 end
 
-get '/ideas/create' do
-  conn = Faraday.new(url: ENV['SERVER']) do |f| # replace 1909 with #{cohort_number} & current_user
-    f.adapter Faraday.default_adapter
-  end
-  # post params to upvote API
-  send_params = request.env['rack.request.query_hash']
-
-  post = conn.post('cohorts/1909/ideas') do |req|
-    req.headers['Content-Type'] = 'application/json'
-    req.params = send_params
-  end
-  # redirect to '/cohorts/:cohort_number/ideas'
-  erb :welcome
-end
+# get '/ideas/create' do
+#   conn = Faraday.new(url: ENV['SERVER']) do |f| # replace 1909 with #{cohort_number} & current_user
+#     f.adapter Faraday.default_adapter
+#   end
+#   # post params to upvote API
+#   send_params = request.env['rack.request.query_hash']
+#
+#   post = conn.post('cohorts/1909/ideas') do |req|
+#     req.headers['Content-Type'] = 'application/json'
+#     req.params = send_params
+#   end
+#   # redirect to '/cohorts/:cohort_number/ideas'
+#   erb :welcome
+# end
