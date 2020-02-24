87e26ef4be5eda60c997731c6125ff401e12480d
Scott Regenthal <scottregenthal@gmail.com>
2020-02-24 10:21:44 -0700

    Move spec to features folder

--- spec/features/upvote_landing_page_spec.rb
+++ spec/features/upvote_landing_page_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'Upvote landing page' do
   it 'displays idea cards' do
     visit '/'
 
-    expect(page).to have_content('Welcome to the Sinatra Template!')
+    expect(page).to have_content('Add an Idea')
   end
 end
