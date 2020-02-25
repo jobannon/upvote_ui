fc1fedf638d4f21c01d89f0b7da64ce01fef8958
Harrison Levin <harrison483@gmail.com>
2020-02-25 12:57:45 -0700

    updates

--- spec/features/ideas/cohort_ideas_spec.rb
+++ spec/features/ideas/cohort_ideas_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe 'Cohorts ideas' do
-  VCR.use_cassette('cohorts/ideas show page') do
     it 'displays all ideas assosiated with a cohort', js: true, type: :feature do
-      visit '/cohorts/1909/ideas'
-      sleep(5)
-      expect(page).to have_content("save the world")
+      visit '/cohorts/ideas'
+
+      sleep(2)
       expect(page).to have_content("Great Idea1")
-      expect(page).to have_content("Great Idea2")
+      expect(page).to have_content("save the world")
+      have_button 'Vote'
+      within ''
     end
-  end
 end
