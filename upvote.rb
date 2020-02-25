2f19f878f3e66d60da8ff351797b3404f8325d23
Scott Regenthal <scottregenthal@gmail.com>
2020-02-24 21:32:55 -0700

    Require ominauth and configure to enable sessions and omniauth::builder

--- upvote.rb
+++ upvote.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'faraday'
 require 'dotenv'
+require 'omniauth-github'
 
 configure do
   set :public_folder, 'public'
   set :views, 'views'
+  enable :sessions
+
+  use OmniAuth::Builder do
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  end
 end
 
 get '/' do
