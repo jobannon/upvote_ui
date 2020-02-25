3286022ddf29ec4aacf8d6c2b46f6eae6963fc78
Scott Regenthal <scottregenthal@gmail.com>
2020-02-24 21:13:13 -0700

    Create login with github test

---
+++ spec/features/user_can_login_with_github_spec.rb
@@ -0,0 +1,26 @@
+# require 'spec_helper'
+#
+# RSpec.describe 'A User' do
+#   it 'can authenticate or login with GitHub OAuth' do
+#     VCR.use_cassette('connect_to_github') do
+#       OmniAuth.config.test_mode = true
+#       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new('provider' => 'github',
+#                                                                   'uid' => '29346170',
+#                                                                   'info' =>
+#         { 'nickname' => 'hale4029',
+#           'email' => 'harrison483@gmail.com',
+#           'name' => 'Harrison',
+#           'image' => 'https://avatars2.githubusercontent.com/u/29346170?v=4',
+#           'urls' => { 'GitHub' => 'https://github.com/hale4029', 'Blog' => '' } },
+#           'credentials' => { 'token' => (ENV['GITHUB_ACCESS_TOKEN']).to_s, 'expires' => false })
+#
+#       visit '/'
+#
+#       expect(page).to have_content('Login with GitHub')
+#       click_link('Login with GitHub')
+#
+#       expect(current_path).to eq('/cohorts/ideas')
+#       expect(page).to have_content('UpVote - A Turing Terrificus Ideation Solution')
+#     end
+#   end
+# end
