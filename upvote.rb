a52ac1db0bc088d22656744487bbe9236557c58b
Scott Regenthal <scottregenthal@gmail.com>
2020-02-25 19:38:12 -0700

    Remove OmniAuth
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>
    Co-Authored-By: Harrison <harrison483@gmail.com>

--- upvote.rb
+++ upvote.rb
@@ -1,16 +1,11 @@
 require 'sinatra'
 require 'faraday'
 require 'dotenv'
-require 'omniauth-github'
 
 configure do
   set :public_folder, 'public'
   set :views, 'views'
   enable :sessions
-
-  use OmniAuth::Builder do
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  end
 end
 
 get '/' do
